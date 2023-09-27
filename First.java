f174d4b2c8b95c4df05a93735488cdf7d221d8ba
Kaviraj124 <umeshgadhavi311@gmail.com>
2023-09-27 18:36:04 +0530

    new file new branch dev

---
+++ First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args)
+    {
+       char i=1;
+       if(i==1){
+        System.out.print("HII");
+    }else{
+         System.out.print("Bye");
+    }
+}
+}
